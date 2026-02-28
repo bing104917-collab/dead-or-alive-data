124be7aba1265b0e8bba3f23aeb4eed756740301
cwtj <448473911@qq.com>
2026-02-28 10:35:25 +0800

    delete android data

--- android/dead-or-alive-data/utils/onnxruntime-node-stub.js
+++
@@ -1,9 +0,0 @@
-﻿// Stub for node-only onnxruntime package to avoid Metro bundling .node binaries.
-// If this gets called in RN, it should fail loudly.
-module.exports = {
-  InferenceSession: {
-    create() {
-      throw new Error('onnxruntime-node is not supported in Expo/RN');
-    },
-  },
-};
