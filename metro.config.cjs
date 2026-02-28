0d15ce0fba8ed85925288bea8b0a7c8b1145a3ab
cwtj <448473911@qq.com>
2026-02-28 11:21:31 +0800

    delete

--- metro.config.cjs
+++
@@ -1,9 +0,0 @@
-﻿const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-config.resolver.assetExts = Array.from(
-  new Set([...(config.resolver.assetExts || []), 'onnx', 'txt'])
-);
-
-module.exports = config;
